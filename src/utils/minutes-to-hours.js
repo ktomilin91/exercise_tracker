f02005ea406cbabdab0e8c7ac064823ec26971cd
ktomilin91 <98376354+ktomilin91@users.noreply.github.com>
2022-01-29 10:49:19 -0800

    Clarified comment in minutes-to-hours.js

--- src/utils/minutes-to-hours.js
+++ src/utils/minutes-to-hours.js
@@ -1,4 +1,4 @@
-// Converts minutes into HH:MM format
+// Converts minutes into a string like 3h 15m
 export default m => {
     const hours = Math.floor(m / 60);
     const minutes = m - (hours * 60);
